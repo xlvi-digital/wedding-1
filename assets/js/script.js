b3f0cb49da7c90e8fdef5b7aacee6599f18ec554
xlvi-digital <reyhanmasrey94@gmail.com>
2025-09-01 14:05:08 +0700

    color change

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // fitur Hitung Mundur
 // Mengatur waktu akhir perhitungan mundur
-var countDownDate = new Date("apr 27, 2025 10:00:00").getTime();
+var countDownDate = new Date("sep 21, 2025 10:00:00").getTime();
 
 // Memperbarui hitungan mundur setiap 1 detik
 var x = setInterval(function () {
